fd5cc9a16c68ab6caddae8b2b752bf818411fb8a
greenTara <taraathan@gmail.com>
2013-04-09 16:43:35 -0400

    removed Joomla software

--- joomla/administrator/components/com_export_content/new_version/uninstall.mysql.utf.sql
+++
@@ -1,7 +0,0 @@
-/**
-* @Component: JMS - Joomla Message System - A Joomla Component.
-* @Copyright: (c) Cas de Vroom. All rights reserved.
-* @Information: See INFORATION.txt for the license, website, etc
-*/
-
-
