4fb39d01b4ae2a822d1dbf755a22932e19522c87
greenTara <taraathan@gmail.com>
2015-05-30 13:11:32 -0400

    recursive scan of files

--- verify/listFiles.php
+++ verify/listFiles.php
@@ -1,8 +1,36 @@
 <?php
+class RecursiveDotFilterIterator extends  RecursiveFilterIterator
+{
+    public function accept()
+    {
+        return '.' !== substr($this->current()->getFilename(), 0, 1);
+    }
+}
 echo "hello\n";
 echo getcwd()."\n";
-$top = getcwd();
-foreach(array_filter(glob($top.'/*'), 'is_file') as $file) {
-    echo "$file\n";
+//$top= $_GET["top"];
+$top = "~/public_html/ruleml.org/verify";
+  
+echo "$top\n";
+
+$iterator = new RecursiveIteratorIterator(
+    new RecursiveDotFilterIterator(
+        new RecursiveDirectoryIterator("$top")
+    )
+);
+foreach($iterator as $file){
+    $path_parts = pathinfo($file);
+    $base = $path_parts['basename'];
+    echo "$base";
+    echo " ";
+    echo $path_parts['dirname'];
+    echo " ";
+    echo filesize($file);
+    echo " ";
+    echo filemtime($file);
+    echo "\n";
 }
-?>
+
+
+
+ ?>
